8d305d10be2c876c80da2d45e68471c9ab6317a2
awasevalentine <54159265+awasevalentine@users.noreply.github.com>
2022-03-13 15:58:13 +0100

    Added configuration of online mysql database and it credentials

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,12 +20,12 @@ import { TransactionsModule } from './modules/transactions/transactions.module';
       config: {
         client: 'mysql',
         connection: {
-          host: '127.0.0.1',
+          host: process.env.MYSQL_HOST,
           port: 3306,
-          user: process.env.MYSQL_USER,
-          password: process.env.MYSQL_PASSWORD,
-          database: 'lendqr',
-          debug: true,
+          user: process.env.MYSQL_USER_2,
+          password: process.env.MYSQL_PASSWORD_2,
+          database: process.env.MYSQL_DB,
+          // debug: true,
         },
         },
     }),
